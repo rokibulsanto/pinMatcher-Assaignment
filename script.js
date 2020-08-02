d4f36e160ca7ed0466eb6545a210f46113e40fd7
rokibulsanto <rhsanto360@gmail.com>
2020-08-02 04:49:58 -0600

    pinMatched assaignment done

--- script.js
+++ script.js
@@ -2,25 +2,53 @@ function calculate(string){
 
     num = parseInt(string);
     document.getElementById("display").value += num;
+    
 
 };
+
 
 function randomNumber(){
 
     num = Math.random() * 10000;
     var result = Math.round(num);
     document.getElementById("pinDisplay").value = result;
-
-   
+    
+ 
 };
+
 
 document.getElementById("submit-area").addEventListener('click',function(){
 
-    if((document.getElementById("display").value) == (document.getElementById("pinDisplay").value)){
+
+    if((document.getElementById("pinDisplay").value && document.getElementById("display").value) == ""){
+
+        document.getElementById("wrong").style.display="block";
+    }
+
+    else if((document.getElementById("display").value) == (document.getElementById("pinDisplay").value)){
 
         document.getElementById("correct").style.display="block";
+        
+
     }
     else{
         document.getElementById("wrong").style.display="block";
+
+        let tries = document.getElementById("tries").innerHTML;
+        let changes = parseInt(tries);
+
+        if(changes>0){
+
+            document.getElementById("tries").innerHTML =  changes - 1;
+        }
+
+        if(changes<=0){
+
+            document.getElementById("actionLeft").innerHTML = "sorry! could not verify the pin";
+            document.getElementById("display").value = "";
+            document.getElementById("pinDisplay").value = "";
+        }
     }
 });
+
+
